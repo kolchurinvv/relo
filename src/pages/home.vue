139f08ecb1d0c509f29f30e19c608e0bdbd62cc0
Vladimir Kolchurin <kolchurin@gmail.com>
2021-10-13 01:11:28 +0500

    removed element ui

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,64 +1,10 @@
 <script setup lang="ts">
-import { ref } from "vue";
+// import { ref } from "vue";
 const mapbox = import.meta.env.VITE_MAPBOX_TOKEN;
-const activeIndex = ref("1");
-const activeIndex2 = ref("1");
-const handleSelect = (key: string, keyPath: string[]): void => {
-  console.log(key, keyPath);
-};
 </script>
 
-<template>
-  //- h1 hello world {{ mapbox }}
-
-  <!-- <el-menu
-    :default-active="activeIndex"
-    class="el-menu-demo"
-    mode="horizontal"
-    @select="handleSelect"
-  >
-    <el-menu-item index="1">Processing Center</el-menu-item>
-    <el-sub-menu index="2">
-      <template #title>Workspace</template>
-      <el-menu-item index="2-1">item one</el-menu-item>
-      <el-menu-item index="2-2">item two</el-menu-item>
-      <el-menu-item index="2-3">item three</el-menu-item>
-      <el-sub-menu index="2-4">
-        <template #title>item four</template>
-        <el-menu-item index="2-4-1">item one</el-menu-item>
-        <el-menu-item index="2-4-2">item two</el-menu-item>
-        <el-menu-item index="2-4-3">item three</el-menu-item>
-      </el-sub-menu>
-    </el-sub-menu>
-    <el-menu-item index="3" disabled>Info</el-menu-item>
-    <el-menu-item index="4">Orders</el-menu-item>
-  </el-menu> -->
-  <div class="line"></div>
-  <el-menu
-    :default-active="activeIndex2"
-    class="el-menu-demo"
-    mode="horizontal"
-    background-color="#545c64"
-    text-color="#fff"
-    active-text-color="#ffd04b"
-    @select="handleSelect"
-  >
-    <el-menu-item index="1">Processing Center</el-menu-item>
-    <el-sub-menu index="2">
-      <template #title>Workspace</template>
-      <el-menu-item index="2-1">item one</el-menu-item>
-      <el-menu-item index="2-2">item two</el-menu-item>
-      <el-menu-item index="2-3">item three</el-menu-item>
-      <el-sub-menu index="2-4">
-        <template #title>item four</template>
-        <el-menu-item index="2-4-1">item one</el-menu-item>
-        <el-menu-item index="2-4-2">item two</el-menu-item>
-        <el-menu-item index="2-4-3">item three</el-menu-item>
-      </el-sub-menu>
-    </el-sub-menu>
-    <el-menu-item index="3" disabled>Info</el-menu-item>
-    <el-menu-item index="4">Orders</el-menu-item>
-  </el-menu>
+<template lang="pug">
+h1 hello world {{ mapbox }}
 </template>
 
 <style lang="sass" scoped></style>
